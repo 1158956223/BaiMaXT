2775c235348d7e1921dfb659d30787d9fbb87360
11589 <1158956223@qq.com>
2026-05-08 16:01:07 +0800

    init commit

---
+++ src/main/java/com/example/BaiMaXtApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaiMaXtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaiMaXtApplication.class, args);
+    }
+
+}
